042d3415faa1662243d6e312eec14d9a69a5b824
ChakaladSArashkami <reaperzerg@gmail.com>
2025-09-06 16:26:49 +0300

    working with ternary operator
    Module04

---
+++ Module04.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main()
+{
+    int num{ 0 };
+    std::cout << "Enter your number : ";
+    std::cin >> num;
+    std::cout << "Answer : "<<(num > 0 && num % 2 == 0 ? num << 2 : num >> 2) << std::endl;
+}

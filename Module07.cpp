76d29d0077be9a5058135d7b977a92b80dba3d50
ChakaladSArashkami <reaperzerg@gmail.com>
2025-09-30 22:25:42 +0300

    Module07
    Module07

---
+++ Module07.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+#include "work.h"
+
+int main()
+{
+	int size{};
+	std::cout << "Enter array size for unique ptr: ";
+	std::cin >> size;
+	auto ptr1 = create_arr_uniq(size);
+	enter_arr(ptr1, size);
+	print_arr(ptr1, size);
+
+	std::cout << "Enter array size for shared ptr: ";
+	std::cin >> size;
+	auto ptr2 = create_arr_sahr(size);
+	enter_arr(ptr2, size);
+	print_arr(ptr2, size);
+
+	sum_weak(ptr2, size);
+
+	int min{100}, max{};
+	min_max(ptr2, size, min, max);
+	std::cout << "Min value: " << min << std::endl;
+	std::cout << "Max value: " << max << std::endl;
+
+	print_overload();
+}
